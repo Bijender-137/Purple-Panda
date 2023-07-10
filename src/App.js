ffe26cb8398321e88c1fc26d4b1481518ad11c2a
antrikshkukreja01 <antrikshkukreja01@gmail.com>
2023-07-10 14:32:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./Ak.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import Contact from "./components/Contact";
+import Hours from "./components/Hours";
+import Footer from "./components/Footer";
+import Panda from "./components/Panda";
+import Potent from "./components/Potent";
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <div className=" overflow-hidden">
+        <Contact />
+        <Hours />
+        <Footer />
+        <Panda />
+        <Hours />
+        <Potent />
+      </div>
+    </>
+  );
 }
 
 export default App;
